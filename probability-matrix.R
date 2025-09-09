9554b8afe470f5341c7891137054c9ea742601f6
gilesdoy <75640623+gilesdoy@users.noreply.github.com>
2025-09-09 16:31:36 +0100

    Update probability-matrix.R

--- probability-matrix.R
+++ probability-matrix.R
@@ -1,8 +1,10 @@
 # Sample list of 100 names
 names_list <- paste("Name", 1:128)
+names_df <- data.frame(nm=names_list,rnk=runif(128))
 
 # Create all combinations of two names (including reversed pairs)
-df <- expand.grid(Name1 = names_list, Name2 = names_list, stringsAsFactors = FALSE)
+#df <- expand.grid(Name1 = names_list, Name2 = names_list, stringsAsFactors = FALSE)
+df <- expand.grid(Name1 = names_df$nm, Name2 = names_df$nm, stringsAsFactors = FALSE)
 
 # Remove rows where Name1 == Name2 (no self-pairs)
 df <- df[df$Name1 != df$Name2, ]
